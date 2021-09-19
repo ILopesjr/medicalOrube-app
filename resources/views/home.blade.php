200df8438c0b064e117ecb59b2a81da7ab914303
Ivanildo Lopes <desenvolvimento8@bisa.com.br>
2021-09-19 17:38:32 -0300

    Add Images

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,15 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Home') }}
-        </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                    <div class="container lex justify-center pt-8 sm:justify-start sm:pt-0">
+                        <img src="{{ url('/images/logo.jpg') }}" alt="Logo" width="800">
+                    </div>
+                </div>
             </div>
         </div>
     </div>
